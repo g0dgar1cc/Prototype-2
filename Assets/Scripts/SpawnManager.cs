ed0f8af2b68824db0c687d73c18f0681f1ce53dc
g0dgar1cc <ting@s.sfusd.edu>
2023-01-30 14:31:04 -0800

    Animals can now spawn randomly from S key

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    public int animalIndex;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +17,8 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+
             Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
         }
     }
